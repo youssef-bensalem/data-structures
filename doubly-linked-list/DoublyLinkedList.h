ad14ea5b331116f783ddadf34cdcad08dc657bb2
youssef-bensalem <youssef23bensalem@gmail.com>
2025-11-23 00:58:39 +0100

    Add header file for DoublyLinkedList structure

--- doubly-linked-list/DoublyLinkedList.h
+++ doubly-linked-list/DoublyLinkedList.h
@@ -1,1 +1,15 @@
+#ifndef DOUBLYLINKEDLIST_H
+#define DOUBLYLINKEDLIST_H
 
+struct Node {
+int data;
+Node* next;
+Node* prev;
+};
+struct List {
+Node* head;
+Node* tail;
+int size;
+};
+
+#endif // DOUBLYLINKEDLIST_H
